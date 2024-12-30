becee5f3198bde2e0c05645bcc08fc7eddcd1c8b
sahilsanger1 <shahilmotog3@gmail.com>
2024-12-30 20:26:27 +0530

    update

--- index.py
+++ index.py
@@ -81,8 +81,8 @@ def reverseString(text):
     # return str
 
 
-str1 = reverseString("Hello Dear")
-print(str1)
+# str1 = reverseString("Hello Dear")
+# print(str1)
 
 # with reversed keyword
 # def reverseKeyword(text):
@@ -93,5 +93,108 @@ print(str1)
 # rev = reverseKeyword("Reverse Me")
 # print(rev)
 
-#! Given a string containing numbers separated by commas (e.g., "1,2,3,4,5"), write a program to calculate the sum of the numbers.
 
+# type conversion
+# int to str
+# num = 23
+# name = str(num)
+# boolean = bool(name)
+# decimal = float(name)
+# print(type(name))
+# print(type(decimal))
+# print(type(boolean))
+
+# name = "Sahil"
+# integer = int(name)
+# print(type(integer))
+
+# implicit type conversion
+
+# res = 3 + 6.4
+# print(res)
+
+# Basic Mathematical Operations
+# import math
+
+# x = 4
+# # print(math.sqrt(x))
+# # print(math.pow(x,3))
+# # print(math.sin(x))
+# # print(math.exp(x))
+# # print(math.log(x))
+
+# y = 9.6
+# print(math.ceil(y))
+# print(math.floor(y))
+# print(math.trunc(y))
+
+# a = "Sahilraj"
+# # print(a[-1])
+# # print(a[1::2])
+# # print(len(a))
+# # loop
+# # for i in a:
+# #     print(i)
+# # repeat
+# # a2 = a * 3
+# # print(a2)
+
+# index = 0
+# while index < len(a):
+#     char = a[index]
+#     print(char)
+#     index += 1
+
+
+# string method
+s = " labrador "
+# print(s.upper())
+# print(s.lower())
+# print(s.capitalize())
+# print(s.strip())
+# print(s.lstrip())
+# print(s.rstrip())
+
+# s = "The quick brown fox"
+# dog = s.split()
+# # print(dog)
+
+# words = ["The", "quick", "brown", "fox"]
+# sentence = " ".join(words)
+# # print(sentence)#
+# index=s.find("fox")
+# print(index)
+
+# str1 = "Emma-is-a-data-scientist"
+# substr = " ".join(str1.split("-"))
+# print(substr)
+
+#! List Practice
+# list is a data type that stores ordered collection of items. key features of list are-
+# 1. Ordered -> list stores data in order manner it means we can store and retrieve data in the same sequence.
+# 2. Mutable -> lists are mutable meaning we can update, remove, add in the same way.
+# 3.Heterogenous -> list can contain different types of data inside it.
+# 4. Dynamic size -> list can grow and shink accordingly
+
+myList = [1, 2, 3, 4, "Data", 55, 34]
+print(myList)
+# accessing a list
+print(myList[2])
+# slicing a list
+# mylist([start:stop:step])
+print(myList[1:3])
+print(myList[:3])
+print(myList[3:])
+print(myList[1::2])
+# updating a list
+myList[4] = "Sahil"
+print(myList)
+
+# list function
+# len()
+print(len(myList))
+# append() -> it adds the item in the last
+myList.append(444)
+# extend() -> The extend() method in Python is used to add all the elements of an iterable (list, tuple, set, etc.) to the end of the current list.
+myList.extend((1, 2, 4))
+print(myList)
